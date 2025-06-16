4dffa7a0aeb52021e8790f3aea73d9bc0632cf2d
John Appiah <jpaappiah@gmail.com>
2025-06-16 15:13:07 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/navbar.jsx'; // Adjust the path as necessary
-import LandingPage from './pages/landingpage.jsx';
+import LandingPage  from './pages/landingpage.jsx';
+import Footer from './components/footer.jsx';
 import About from './pages/about.jsx';
 
 function App() {
 
   return (
     <Router>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </Router>
+
   )
 }
 
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+function Footer(){
+    return(
+        <div className="footer">
+            <p>
+                Copyright @ 2025  MentorMe, All rights reserved.
+                Customer Service
+                Terms & Conditions
+                Privacy Policy
+            </p>
+        </div>
+    )
+}
+
+export default Footer
