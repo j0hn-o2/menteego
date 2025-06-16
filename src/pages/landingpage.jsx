af488b28f1002047bfe23198e548bdbb83445826
John Appiah <jpaappiah@gmail.com>
2025-06-16 16:48:13 +0000

    third commit

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -10,7 +10,11 @@ function LandingPage() {
             <div className="section1">
                 <div className="section1-text">
                     <h2> FIND THE RIGHT MENTOR FOR YOU</h2>
-                    <p>Connect with expereienced mentors for guidance and support in your personal and professional growth.</p>
+                    <p>
+                    Connect with expereienced mentors for guidance and support in your personal and professional growth. 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero culpa soluta, alias repellendus officiis 
+                    sunt delectus numquam sapiente quae ab corporis doloribus, id nihil odit porro odio nulla reprehenderit deleniti.
+                    </p>
                     
                     <button>Login</button>
                     <button>Create Account</button>
@@ -27,7 +31,12 @@ function LandingPage() {
                     <h2>FEATURES</h2>
                 </div>
 
-                <div className="section2-feature1">
+                
+
+                <div className="section2-features">
+                    <div className="dark-overlay"></div>
+
+                    <div className="section2-feature1">
                     <div className="section2-image">
                         <img src={image1} alt="Image" />       
                     </div>
@@ -68,10 +77,17 @@ function LandingPage() {
                 <div className="section2-feature3-text">
                     <h3>JOIN DISCUSSION GROUPS</h3>    
 
-                    <p>Share and participate within your community to learn about the latest trends.</p>  
+                    <p>
+                        Share and participate within your community to learn about the latest trends.
+                        
+                    </p>  
 
                 </div>
 
+
+                </div>
+
+                
                 </div>
             </div>
 
@@ -79,6 +95,74 @@ function LandingPage() {
                 <div className="section3-header">
                     <h2>HEAR FROM OUR ALUMNI</h2>
                 </div>
+
+                <div className="section3-reviews">
+
+                    <div className="section3-review1">
+                    <div className="review-text">
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Quidem voluptatum sit accusantium eos mollitia ducimus nobis quod nihil. Eius nemo necessitatibus, exercitationem labore itaque expedita 
+                        officia enim veritatis maiores fugit corporis molestiae iure, dicta quis minima. Doloribus praesentium dolorum perspiciatis, 
+                        beatae asperiores tenetur nemo reiciendis vitae est ratione nostrum! Et?
+                        </p>
+                    </div>
+                    <div className="review-profile">
+                        <div className="review-image">
+                        <img src={image1} alt="Image" />
+                        </div>
+                        <p className='review-name'>
+                            John Doe<br/> Software Engineer
+                        </p>
+                    </div>
+
+
+                </div>
+                <div className="section3-review1">
+                    <div className="review-text">
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Quidem voluptatum sit accusantium eos mollitia ducimus nobis quod nihil. Eius nemo necessitatibus, exercitationem labore itaque expedita 
+                        officia enim veritatis maiores fugit corporis molestiae iure, dicta quis minima. Doloribus praesentium dolorum perspiciatis, 
+                        beatae asperiores tenetur nemo reiciendis vitae est ratione nostrum! Et?
+                        </p>
+                    </div>
+                    <div className="review-profile">
+                        <div className="review-image">
+                        <img src={image1} alt="Image" />
+                        </div>
+                        <p className='review-name'>
+                            John Doe<br/> Software Engineer
+
+                        </p>
+                    </div>
+
+
+                </div>
+                <div className="section3-review1">
+                    <div className="review-text">
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Quidem voluptatum sit accusantium eos mollitia ducimus nobis quod nihil. Eius nemo necessitatibus, exercitationem labore itaque expedita 
+                        officia enim veritatis maiores fugit corporis molestiae iure, dicta quis minima. Doloribus praesentium dolorum perspiciatis, 
+                        beatae asperiores tenetur nemo reiciendis vitae est ratione nostrum! Et?
+                        </p>
+                    </div>
+                    <div className="review-profile">
+                        <div className="review-image">
+                        <img src={image1} alt="Image" />
+                        </div>
+                        <p className='review-name'>
+                            John Doe<br/> Software Engineer
+                        </p>
+                    </div>
+
+                </div>
+
+               </div>
+
+
+                
 
             </div>
 
